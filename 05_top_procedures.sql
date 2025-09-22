f62d2e8bc4bf707941c5d0cf3f7157fba6fa0b87
Nick Fu <nick.h.fu@gmail.com>
2025-09-22 13:53:26 -0500

    Update 05_top_procedures.sql

--- 05_top_procedures.sql
+++ 05_top_procedures.sql
@@ -37,93 +37,81 @@ Recommendations:
 
 /*
 -------------------------------------------------------
-(1) What are the top 5 procedures performed by age group?
+(1) What are the top 5 procedures performed by each age group?
 -------------------------------------------------------
 */
 
-with age_group_procedures as(
-	select
+WITH age_group_procedures AS(
+	SELECT
 		encount.encounter_Id,
-		encount.encounter_class,
-		case
-			when pat.death_date is null then datediff(year, pat.birth_date, getdate())
-			when pat.death_date is not null then datediff(year, pat.birth_date, pat.death_date) end
-		 as age,
-		 case
-			when datediff(year, pat.birth_date, getdate()) between 30 and 40 then '30-40'
-			when datediff(year, pat.birth_date, getdate()) between 40 and 50 then '40-50'
-			when datediff(year, pat.birth_date, getdate()) between 50 and 60 then '50-60'
-			when datediff(year, pat.birth_date, getdate()) between 60 and 70 then '60-70'
-			when datediff(year, pat.birth_date, getdate()) between 70 and 80 then '70-80'
-			when datediff(year, pat.birth_date, getdate()) > 80 then '80+' end
-		as age_group,
+		v.age_at_encounter AS age,
+		v.age_group_at_encounter AS age_group,
 		prod.procedure_code,
 		prod.description
-	from encounters encount
-		left join patients pat
-			on encount.patient_id = pat.PATIENT_ID
-		left join procedures prod
-			on encount.encounter_id = prod.ENCOUNTER_ID
-)
+	FROM encounters encount
+		LEFT JOIN procedures prod
+			ON encount.encounter_id = prod.encounter_id
+		JOIN dbo.vw_encounter_age v
+			ON encount.encounter_id = v.encounter_id
+		WHERE prod.procedure_code IS NOT NULL
+),
 
-select
+ranked_procedures as(
+SELECT
 	age_group,
 	procedure_code,
 	description,
-	total_encounters,
-	ranking
-from
-	(
-	select
-		age_group,
-		procedure_code,
-		description,
-		count(distinct encounter_id) as total_encounters,
-		dense_rank() over(partition by age_group order by count(distinct encounter_id) desc) as ranking
-	from age_group_procedures
-	where procedure_code is not null
-	group by age_group,procedure_code, description
-	) Ranked_Procedures
-where ranking <= 5;
+	COUNT(DISTINCT encounter_id) as total_encounters,
+	DENSE_RANK() OVER(PARTITION BY age_group ORDER BY COUNT(DISTINCT encounter_id) DESC) as ranking
+FROM age_group_procedures
+GROUP BY age_group, procedure_code, description
+)
+
+
+SELECT
+	age_group,
+	procedure_code,
+	description,
+	total_encounters
+FROM ranked_procedures
+WHERE ranking <=5;
 
 
 /*
 -------------------------------------------------------
-(2) What are the top 10 procedures performed by gender?
+(2) What are the top 5 procedures performed by gender?
 -------------------------------------------------------
 */
 
-with gender_procedures as(
-	select
-		encount.encounter_Id,
-		encount.encounter_class,
-		pat.gender,
-		prod.procedure_code,
-		prod.description
-	from encounters encount
-		left join patients pat
-			on encount.patient_id = pat.PATIENT_ID
-		left join procedures prod
-			on encount.encounter_id = prod.ENCOUNTER_ID
-)
+WITH gender_procedures AS(
+SELECT
+	encount.encounter_Id,
+	pat.gender,
+	prod.procedure_code,
+	prod.description
+FROM encounters encount
+	LEFT JOIN patients pat
+		ON encount.patient_id = pat.patient_id
+	LEFT JOIN procedures prod
+		ON encount.encounter_id = prod.encounter_ID
+		WHERE prod.procedure_code IS NOT NULL
+),
 
-select
+ranked_procedures AS (
+SELECT
 	gender,
 	procedure_code,
 	description,
-	total_encounters,
-	ranking
-from
-	(
-	select
-		gender,
-		procedure_code,
-		description,
-		count(distinct encounter_id) as total_encounters,
-		dense_rank() over(partition by gender order by count(distinct encounter_id) desc) as ranking
-	from gender_procedures
-	where procedure_code is not null
-	group by gender,procedure_code, description
-	) Ranked_Procedures
-where ranking <= 10
-order by gender desc;
+	COUNT(DISTINCT encounter_id) AS total_encounters,
+	DENSE_RANK() OVER(PARTITION BY gender ORDER BY COUNT(DISTINCT encounter_id) DESC) AS ranking
+FROM gender_procedures
+GROUP BY gender, procedure_code, description
+)
+
+SELECT
+	gender,
+	procedure_code,
+	description,
+	total_encounters
+FROM ranked_procedures
+WHERE ranking <=5;
