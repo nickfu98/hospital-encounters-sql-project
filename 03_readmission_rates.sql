6315c1e6a33184d9040644fe67c8b1d9fa11e830
Nick Fu <nick.h.fu@gmail.com>
2025-09-22 13:50:45 -0500

    Update 03_readmission_rates.sql

--- 03_readmission_rates.sql
+++ 03_readmission_rates.sql
@@ -12,14 +12,14 @@ Business Questions:
 
 
 Key Findings:
-(1) The overall 30-day readmission rate is 61.58%. 
+(1) The overall 30-day readmission rate is 62.35%. 
 (2) Patients over the age of 80 have the highest rates of readmission by far at 69.46% with 60-70 year olds with the least amount of readmission (41.93%).
 (3) Urgent care visits have the highest rates of readmission at 74.14% and wellness visits have the lowest rates of readmission at 21.28%.
 (4) Patients with no insurance have the highest rates of readmission at 76.83% and those with Anthem insurance have the lowest rates of readmission at 31.35%.
 NOTE: These RR% would be EXTREMELY high in a real world hospital, however coming from a synthetic dataset, the numbers do not reflect reality.
 
 Recommendations:
-(1) Overall Readmission Rate 61.58%
+(1) Overall Readmission Rate 62.35%
 	- Make sure patients clearly understand their care and what to do after leaving to reduce readmissions
 	- Keep in touch with patients after discharge for early detection of any problems causing them to readmit
 (2) Readmission by Age Group - 80+
@@ -40,41 +40,42 @@ Recommendations:
 -------------------------------------------------------
 */
 
-with encounters_order as (
-	select
-		encount.patient_id,
-		cast(encount.start as date) date,
-		cast(encount.stop as date) discharge_date,
-		encount.encounter_id,
-		lag(encount.stop) over (partition by encount.patient_id order by cast(encount.start as date)) as previous_discharge_date,
-		datediff(day, lag(encount.stop) over (partition by encount.patient_id order by encount.start), start) days_since_discharge,
-		row_number() over(Partition by encount.patient_id order by encount.start) encounter_number
-	from encounters encount
-		left join patients pat
-		on encount.patient_id = pat.patient_id
-	where stop is not null
+WITH encounters_ordered AS (
+SELECT
+	encount.encounter_id,
+    encount.patient_id,
+	encount.encounter_class,
+	pay.payer_name,
+	v.age_group_at_encounter,
+	v.age_group_sort,
+    CAST(encount.start AS DATE) AS start_date,
+    CAST(encount.stop  AS DATE) AS stop_date,
+    LEAD(CAST(encount.start AS DATE)) OVER (PARTITION BY encount.patient_id ORDER BY encount.start) AS next_start_date
+FROM dbo.encounters encount
+	LEFT JOIN dbo.payers pay
+		ON encount.payer_id = pay.payer_id
+	JOIN dbo.vw_encounter_age v
+		ON encount.encounter_id = v.encounter_id
+WHERE encount.stop IS NOT NULL
+),
+
+readmissions AS (
+SELECT
+	encounter_id,
+	encounter_class,
+	payer_name,
+	age_group_at_encounter,
+	age_group_sort,
+    CASE
+		WHEN next_start_date IS NOT NULL AND DATEDIFF(DAY, stop_date, next_start_date) <= 30 THEN 1 
+		ELSE 0
+    END AS yes_readmit_30d
+FROM encounters_ordered
 )
 
-,Readmissions as (
-	select
-		now.patient_id,
-		now.encounter_id as index_encounter_id,
-		next.encounter_id as readmission_encounter_id,
-		datediff(day, now.discharge_date, next.date) as days_between
-	from encounters_order now
-		join encounters_order next
-		on now.patient_id = next.patient_id
-		and next.encounter_number = now.encounter_number + 1
-		and datediff(day, now.discharge_date, next.date) between 0 and 30
-)
-
-select
-	count(distinct readmit.index_encounter_id) as number_readmissions,
-	count(distinct encounto.encounter_id) as total_index_encounters,
-	concat(round(100.0*cast(count(distinct readmit.index_encounter_id) as float) / count(distinct encounto.encounter_id), 2), '%') as readmission_rate_30_day
-from encounters_order encounto
-	left join readmissions readmit
-	on encounto.encounter_id = readmit.index_encounter_id;
+SELECT 
+	CAST(100.0 * SUM(yes_readmit_30d) / COUNT(*) AS DECIMAL(5,2)) AS readmission_rate_30d
+FROM readmissions;
 
 
 /*
@@ -83,57 +84,47 @@ from encounters_order encounto
 -------------------------------------------------------
 */
 
-with encounters_order as (
-	select
-		encount.patient_id,
-		cast(encount.start as date) date,
-		cast(encount.stop as date) discharge_date,
-		encount.encounter_id,
-		case
-			when pat.death_date is null then datediff(year, pat.birth_date, getdate())
-			when pat.death_date is not null then datediff(year, pat.birth_date, pat.death_date) end
-			as age,
-			case
-			when datediff(year, pat.birth_date, getdate()) between 30 and 40 then '30-40'
-			when datediff(year, pat.birth_date, getdate()) between 40 and 50 then '40-50'
-			when datediff(year, pat.birth_date, getdate()) between 50 and 60 then '50-60'
-			when datediff(year, pat.birth_date, getdate()) between 60 and 70 then '60-70'
-			when datediff(year, pat.birth_date, getdate()) between 70 and 80 then '70-80'
-			when datediff(year, pat.birth_date, getdate()) > 80 then '80+' end
-		as age_group,
-		lag(encount.stop) over (partition by encount.patient_id order by cast(encount.start as date)) as previous_discharge_date,
-		datediff(day, lag(encount.stop) over (partition by encount.patient_id order by encount.start), start) days_since_discharge,
-		row_number() over(Partition by encount.patient_id order by encount.start) encounter_number
-	from encounters encount
-		left join patients pat
-		on encount.patient_id = pat.patient_id
-	where stop is not null
+WITH encounters_ordered AS (
+SELECT
+	encount.encounter_id,
+    encount.patient_id,
+	encount.encounter_class,
+	pay.payer_name,
+	v.age_group_at_encounter,
+	v.age_group_sort,
+    CAST(encount.start AS DATE) AS start_date,
+    CAST(encount.stop  AS DATE) AS stop_date,
+    LEAD(CAST(encount.start AS DATE)) OVER (PARTITION BY encount.patient_id ORDER BY encount.start) AS next_start_date
+FROM dbo.encounters encount
+	LEFT JOIN dbo.payers pay
+		ON encount.payer_id = pay.payer_id
+	JOIN dbo.vw_encounter_age v
+		ON encount.encounter_id = v.encounter_id
+WHERE encount.stop IS NOT NULL
+),
+
+readmissions AS (
+SELECT
+	encounter_id,
+	encounter_class,
+	payer_name,
+	age_group_at_encounter,
+	age_group_sort,
+    CASE
+		WHEN next_start_date IS NOT NULL AND DATEDIFF(DAY, stop_date, next_start_date) <= 30 THEN 1 
+		ELSE 0
+    END AS yes_readmit_30d
+FROM encounters_ordered
 )
 
-,Readmissions as (
-	select
-		now.age_group,
-		now.patient_id,
-		now.encounter_id as index_encounter_id,
-		next.encounter_id as readmission_encounter_id,
-		datediff(day, now.discharge_date, next.date) as days_between
-	from encounters_order now
-		join encounters_order next
-		on now.patient_id = next.patient_id
-		and next.encounter_number = now.encounter_number + 1
-		and datediff(day, now.discharge_date, next.date) between 0 and 30
-)
-
-select
-	encounto.age_group,
-	count(distinct readmit.index_encounter_id) as number_readmissions,
-	count(distinct encounto.encounter_id) as total_index_encounters,
-	concat(round(100.0*cast(count(distinct readmit.index_encounter_id) as float) / count(distinct encounto.encounter_id), 2), '%') as readmission_rate_30_day
-from encounters_order encounto
-	left join readmissions readmit
-	on encounto.encounter_id = readmit.index_encounter_id
-group by encounto.age_group
-order by encounto.age_group
+SELECT
+	age_group_at_encounter AS age_group,
+	SUM(yes_readmit_30d) AS number_readmissions,
+	COUNT(*) as total_encounters,
+	CAST(100.0 * sum(yes_readmit_30d) / COUNT(*) AS DECIMAL(5,2)) AS readmission_rate_30d
+FROM readmissions
+GROUP BY age_group_at_encounter, age_group_sort
+ORDER BY age_group_sort;
 
 /*
 -------------------------------------------------------
@@ -141,46 +132,47 @@ order by encounto.age_group
 -------------------------------------------------------
 */
 	
-with encounters_order as (
-	select
-		encount.patient_id,
-		cast(encount.start as date) date,
-		cast(encount.stop as date) discharge_date,
-		encount.encounter_id,
-		encount.encounter_class,
-		lag(encount.stop) over (partition by encount.patient_id order by cast(encount.start as date)) as previous_discharge_date,
-		datediff(day, lag(encount.stop) over (partition by encount.patient_id order by encount.start), start) days_since_discharge,
-		row_number() over(Partition by encount.patient_id order by encount.start) encounter_number
-	from encounters encount
-		left join patients pat
-		on encount.patient_id = pat.patient_id
-	where stop is not null
+WITH encounters_ordered AS (
+SELECT
+	encount.encounter_id,
+    encount.patient_id,
+	encount.encounter_class,
+	pay.payer_name,
+	v.age_group_at_encounter,
+	v.age_group_sort,
+    CAST(encount.start AS DATE) AS start_date,
+    CAST(encount.stop  AS DATE) AS stop_date,
+    LEAD(CAST(encount.start AS DATE)) OVER (PARTITION BY encount.patient_id ORDER BY encount.start) AS next_start_date
+FROM dbo.encounters encount
+	LEFT JOIN dbo.payers pay
+		ON encount.payer_id = pay.payer_id
+	JOIN dbo.vw_encounter_age v
+		ON encount.encounter_id = v.encounter_id
+WHERE encount.stop IS NOT NULL
+),
+
+readmissions AS (
+SELECT
+	encounter_id,
+	encounter_class,
+	payer_name,
+	age_group_at_encounter,
+	age_group_sort,
+    CASE
+		WHEN next_start_date IS NOT NULL AND DATEDIFF(DAY, stop_date, next_start_date) <= 30 THEN 1 
+		ELSE 0
+    END AS yes_readmit_30d
+FROM encounters_ordered
 )
 
-,Readmissions as (
-	select
-		now.encounter_class, 
-		now.patient_id,
-		now.encounter_id as index_encounter_id,
-		next.encounter_id as readmission_encounter_id,
-		datediff(day, now.discharge_date, next.date) as days_between
-	from encounters_order now
-		join encounters_order next
-		on now.patient_id = next.patient_id
-		and next.encounter_number = now.encounter_number + 1
-		and datediff(day, now.discharge_date, next.date) between 0 and 30
-)
-
-select
-	encounto.encounter_class,
-	count(distinct readmit.index_encounter_id) as number_readmissions,
-	count(distinct encounto.encounter_id) as total_index_encounters,
-	concat(round(100.0*cast(count(distinct readmit.index_encounter_id) as float) / count(distinct encounto.encounter_id), 2), '%') as readmission_rate_30_day
-from encounters_order encounto
-	left join readmissions readmit
-	on encounto.encounter_id = readmit.index_encounter_id
-group by encounto.encounter_class
-order by readmission_rate_30_day desc
+SELECT
+	encounter_class,
+	SUM(yes_readmit_30d) readmissions_number,
+	COUNT(*) as total_encounters,
+	CAST(100.0 * SUM(yes_readmit_30d) / COUNT(*) AS DECIMAL(5,2)) AS readmission_rate_30d
+FROM readmissions
+GROUP BY encounter_class
+ORDER BY readmission_rate_30d DESC
 
 
 /*
@@ -189,45 +181,44 @@ order by readmission_rate_30_day desc
 -------------------------------------------------------
 */
 	
-with encounters_order as (
-	select
-		encount.patient_id,
-		cast(encount.start as date) date,
-		cast(encount.stop as date) discharge_date,
-		encount.encounter_id,
-		pay.payer_name,
-		lag(encount.stop) over (partition by encount.patient_id order by cast(encount.start as date)) as previous_discharge_date,
-		datediff(day, lag(encount.stop) over (partition by encount.patient_id order by encount.start), start) days_since_discharge,
-		row_number() over(Partition by encount.patient_id order by encount.start) encounter_number
-	from encounters encount
-		left join patients pat
-		on encount.patient_id = pat.patient_id
-		left join payers pay
-		on encount.payer_id = pay.payer_id
-	where stop is not null
+WITH encounters_ordered AS (
+SELECT
+	encount.encounter_id,
+    encount.patient_id,
+	encount.encounter_class,
+	pay.payer_name,
+	v.age_group_at_encounter,
+	v.age_group_sort,
+    CAST(encount.start AS DATE) AS start_date,
+    CAST(encount.stop  AS DATE) AS stop_date,
+    LEAD(CAST(encount.start AS DATE)) OVER (PARTITION BY encount.patient_id ORDER BY encount.start) AS next_start_date
+FROM dbo.encounters encount
+	LEFT JOIN dbo.payers pay
+		ON encount.payer_id = pay.payer_id
+	JOIN dbo.vw_encounter_age v
+		ON encount.encounter_id = v.encounter_id
+WHERE encount.stop IS NOT NULL
+),
+
+readmissions AS (
+SELECT
+	encounter_id,
+	encounter_class,
+	payer_name,
+	age_group_at_encounter,
+	age_group_sort,
+    CASE
+		WHEN next_start_date IS NOT NULL AND DATEDIFF(DAY, stop_date, next_start_date) <= 30 THEN 1 
+		ELSE 0
+    END AS yes_readmit_30d
+FROM encounters_ordered
 )
 
-,Readmissions as (
-	select
-		now.payer_name,
-		now.patient_id,
-		now.encounter_id as index_encounter_id,
-		next.encounter_id as readmission_encounter_id,
-		datediff(day, now.discharge_date, next.date) as days_between
-	from encounters_order now
-		join encounters_order next
-		on now.patient_id = next.patient_id
-		and next.encounter_number = now.encounter_number + 1
-		and datediff(day, now.discharge_date, next.date) between 0 and 30
-)
-
-select
-	encounto.payer_name,
-	count(distinct readmit.index_encounter_id) as number_readmissions,
-	count(distinct encounto.encounter_id) as total_index_encounters,
-	concat(round(100.0*cast(count(distinct readmit.index_encounter_id) as float) / count(distinct encounto.encounter_id), 2), '%') as readmission_rate_30_day
-from encounters_order encounto
-	left join readmissions readmit
-	on encounto.encounter_id = readmit.index_encounter_id
-group by encounto.payer_name
-order by readmission_rate_30_day desc;
+SELECT
+	payer_name,
+	SUM(yes_readmit_30d) readmissions_number,
+	COUNT(*) as total_encounters,
+	CAST(100.0 * SUM(yes_readmit_30d) / COUNT(*) AS DECIMAL(5,2)) AS readmission_rate_30d
+FROM readmissions
+GROUP BY payer_name
+ORDER BY readmission_rate_30d DESC;
